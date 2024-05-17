4c1ae82051ff1090af7035bb66d9783354bf1817
nimrosh <nimrodka@mta.ac.il>
2024-05-17 23:14:58 +0300

    first commit

--- back/app.py
+++ back/app.py
@@ -1,23 +1,53 @@
-from flask import Flask, jsonify, request, redirect, url_for
+from flask import Flask, jsonify, request, session
 from flask_cors import CORS
+from users_crud import *
+from werkzeug.security import generate_password_hash ,check_password_hash
+import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 CORS(app)
 
-users = {}  # A dictionary to store user data
+users = {} 
+
+load_dotenv()
+app.secret_key = os.environ.get('SECRET_KEY')
 
 @app.route('/', methods=['GET'])
 def health_check():
-    print(users)
+    print(session)
     return jsonify('connected to server...'), 200
-
 
 @app.route('/register', methods=['POST'])
 def register_details():
     email = request.json.get('email')
     username = request.json.get('username')
     password = request.json.get('password')
-    if email in users:
-        return jsonify({"message": "Email already exists"}), 400
-    users[email] = [username, password]
+    message = username_email_exists(username, email)
+    print(message)
+    if message:
+        return jsonify({"message": message}), 400
+    password_hash = generate_password_hash(password)  
+    add_user(email, username, password_hash)  
     return jsonify({"message": "Registration complete"}), 200
+
+@app.route('/login', methods=['POST'])
+def login_details():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    print(username, password)
+    if get_user_password(username):
+        data_base_password = get_user_password(username)
+        print(data_base_password)
+        if check_password_hash(data_base_password, password):
+            session['username'] = username 
+            return jsonify({"message": "Login successful"}), 200
+        else:
+            return jsonify({"message": "Invalid password"}), 400
+    else:
+        return jsonify({"message": "Invalid username"}), 400
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return jsonify({"message": "Logout successful"}), 200
---
+++ back/users_crud.py
@@ -0,0 +1,56 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+cred = credentials.Certificate(r"C:\Users\nknim\Desktop\uni-drive\back\reddit-clone-fdade-firebase-adminsdk-80zb3-a920ba2736.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+users = db.collection('users')
+
+def add_user(email, username, password):
+    user = users.document(username)
+    if user.get().exists:
+        return {"message": "username already exists"}, 400
+    user.set({"email": email, "password": password})
+    return {"message": "Registration complete"}, 200
+
+
+def get_user(username):
+    user = users.document(username)
+    if not user.get().exists:
+        return False
+    return user.get().to_dict()
+
+
+def get_user_email(username):
+    user = users.document(username)
+    if not user.get().exists:
+        return False
+    return user.get().to_dict()["email"]
+
+
+def get_user_password(username):
+    user = users.document(username)
+    if not user.get().exists:
+        return False
+    return user.get().to_dict()["password"]
+
+def email_exists(email):
+    for user in users.stream():
+        if user.to_dict()["email"] == email:
+            return True
+    return False
+
+def username_exists(username):
+    for user in users.stream():
+        if user.id == username:
+            return True
+    return False
+
+
+def username_email_exists(username, email):
+    for user in users.stream():
+        if user.id == username:
+            return "username already exists"
+        elif user.to_dict()["email"] == email:
+            return "email already exists"
+    return False
